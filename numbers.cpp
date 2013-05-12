de75a6e4a892d5e728c9c04ea63a31cdd7738d34
Jesse Huang <jeshuang@eden.rutgers.edu>
2013-05-11 23:39:11 -0400

    finished power mod function, woring on miller rabin

--- numbers.cpp
+++ numbers.cpp
@@ -3,33 +3,61 @@
 
 using namespace std;
 typedef unsigned long long ull;
-
+unsigned powmod(ull base, ull exponent, ull modulus);
 bool isPrime(ull x)
 {
-     if (x < 2)
-          return false;
-     ull d = x - 1;
-     ull s = 0;
-     ull a;
-     
-     // express x -1 as 2^s * d
-     while (d % 2 == 0)
-     {
-          d /= 2;
-          s++;
-     }
-     for (int k = 0; k < 100; k++)
-     {
-          a = rand() % (x - 4) + 2;
-          
-     }
+	if (x < 2)
+		return false;
+	ull d = x - 1;
+	ull s = 0;
+	ull a;
+	ull t;
+
+	// express x -1 as 2^s * d
+	while (d % 2 == 0)
+	{
+		d /= 2;
+		s++;
+	}
+	for (int k = 0; k < 100; k++)
+	{
+		a = rand() % (x - 4) + 2;
+		t = powmod(a, d, x);
+		if (t == 1 || t == x-1)
+			continue;
+		for (int l; l < s-1; l++)
+		{
+			t = powmod(t, 2, x);
+			if (t == 1)
+				return false;
+			if (t == x-1)
+				break;
+		}
+	cout << "hi" << endl;
+		if (t == x-1)
+			continue;
+		return false;
+	}
+	return true;
 
 }
 
-unsigned powmod(ull a, ull b, ull n)
+unsigned powmod(ull base, ull exponent, ull modulus)
+{
+	ull answer = 1;
+	while (exponent > 0)
+	{
+		if (exponent % 2 == 1)
+			answer = (answer * base) % modulus;
+		exponent >>= 1;
+		base = (base * base) % modulus;
+
+	}
+	return answer;
+}
 
 int main()
 {
-//     isPrime(9);
-     return 0;
+	cout << isPrime(100) << "," << isPrime(97) << endl;
+	return 0;
 }
